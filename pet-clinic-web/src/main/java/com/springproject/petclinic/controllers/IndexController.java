183cd46d38649ef26e8dbab3248edf2e06b5eae4
omerbegovic <omerbegovic.mustafa@gmail.com>
2020-10-11 01:42:09 +0200

    Apply Master Layout to Index Page. close #18

--- pet-clinic-web/src/main/java/com/springproject/petclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/springproject/petclinic/controllers/IndexController.java
@@ -1,10 +1,11 @@
 package com.springproject.petclinic.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
+
     @RequestMapping({"", "/", "index", "index.html"})
     public String index(){
         return "index";
