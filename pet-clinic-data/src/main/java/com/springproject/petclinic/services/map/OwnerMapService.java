3b871381bd6b364af94962eb637d1c6b24d3a5cb
omerbegovic <mustafa.visoko@hotmail.com>
2020-10-27 19:50:44 +0100

    Spring Data JPA Service for Specialities. close #43

--- pet-clinic-data/src/main/java/com/springproject/petclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/com/springproject/petclinic/services/map/OwnerMapService.java
@@ -17,12 +17,12 @@ import java.util.Set;
 @ComponentScan
 @EnableJpaRepositories
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService{
+public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService{
 
     private final PetService petService;
     private final PetTypeService petTypeService;
 
-    public OwnerServiceMap(PetService petService, PetTypeService petTypeService) {
+    public OwnerMapService(PetService petService, PetTypeService petTypeService) {
         this.petService = petService;
         this.petTypeService = petTypeService;
     }
